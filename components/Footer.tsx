ee8870d25be149ed4ddca1b9143669b33af31362
lsc58461 <lsc58461@naver.com>
2024-04-15 23:56:01 +0900

    feat: implement footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import FooterLinks from './FooterLinks';
+
+function Footer() {
+  return (
+    <div className='gap-10pxr flex-col-center'>
+      <p className='text-[#9e9e9e]'>
+        Copyright by 2024. 이정윤. All Rights Reserved
+      </p>
+      <FooterLinks />
+    </div>
+  );
+}
+
+export default Footer;
