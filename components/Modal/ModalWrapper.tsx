d3cd7335449903d7d0d9c5ecaa0eec5621a55fd5
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2024-04-16 18:21:13 +0900

    fix: modal vh not working properly in mobile environment (#28)

--- components/Modal/ModalWrapper.tsx
+++ components/Modal/ModalWrapper.tsx
@@ -13,7 +13,7 @@ interface Props {
 function ModalWrapper({ onCloseClick, children }: Props) {
   return (
     <div
-      className='absolute left-1/2 top-1/2 w-full max-w-1240pxr -translate-x-1/2 -translate-y-1/2 rounded-[14px] bg-[#323232] px-70pxr py-40pxr shadow-[0px_0px_250px_0px_rgba(0,0,0,0.80)] mobile:left-0pxr mobile:top-0pxr mobile:h-screen mobile:w-screen mobile:-translate-x-0pxr mobile:-translate-y-0pxr mobile:rounded-none mobile:px-20pxr mobile:py-55pxr tablet:left-0pxr tablet:top-0pxr tablet:h-screen tablet:w-screen tablet:-translate-x-0pxr tablet:-translate-y-0pxr tablet:rounded-none tablet:p-55pxr tablet:px-20pxr'
+      className='absolute left-1/2 top-1/2 w-full max-w-1240pxr -translate-x-1/2 -translate-y-1/2 rounded-[14px] bg-[#323232] px-70pxr py-40pxr shadow-[0px_0px_250px_0px_rgba(0,0,0,0.80)] mobile:left-0pxr mobile:top-0pxr mobile:h-svh mobile:w-screen mobile:-translate-x-0pxr mobile:-translate-y-0pxr mobile:rounded-none mobile:px-20pxr mobile:py-55pxr tablet:left-0pxr tablet:top-0pxr tablet:h-svh tablet:w-screen tablet:-translate-x-0pxr tablet:-translate-y-0pxr tablet:rounded-none tablet:p-55pxr tablet:px-20pxr'
       onClick={(e) => e.stopPropagation()}
     >
       {children}
