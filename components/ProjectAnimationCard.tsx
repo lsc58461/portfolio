c41c8b734fa5c4bb329e5b4e2df00b1b2fb01839
lsc58461 <lsc58461@naver.com>
2024-04-17 17:42:17 +0900

    feat: link image to card component

--- components/ProjectAnimationCard.tsx
+++ components/ProjectAnimationCard.tsx
@@ -1,11 +1,25 @@
+'use client';
+
+import { useDelayedCardImage } from '../hooks';
+import Image from 'next/image';
+
 interface Props {
   order: number;
   customStyle?: string;
   name: string;
   type: string;
+  imageSrc: string;
 }
 
-function ProjectAnimationCard({ order, customStyle, name, type }: Props) {
+function ProjectAnimationCard({
+  order,
+  customStyle,
+  name,
+  type,
+  imageSrc,
+}: Props) {
+  const { imageUrl } = useDelayedCardImage({ prevUrl: imageSrc, delay: 500 });
+
   const orderByClassName = () => {
     switch (order) {
       case 1:
@@ -23,7 +37,9 @@ function ProjectAnimationCard({ order, customStyle, name, type }: Props) {
     <div
       className={`${orderByClassName()} ${customStyle} absolute left-18pxr flex h-352pxr w-264pxr flex-col items-center justify-start gap-60pxr rounded-[14px] border border-[rgba(248,248,248,0.30)] bg-[rgba(181,181,181,0.50)] p-35pxr shadow-projectCards  backdrop-blur-[5px] mobile:h-252pxr mobile:w-164pxr mobile:gap-30pxr mobile:px-18pxr mobile:py-20pxr`}
     >
-      <div className='firstBox h-124pxr w-full rounded-[14px] bg-white mobile:h-90pxr' />
+      <div className='firstBox relative h-124pxr w-full overflow-hidden rounded-[14px] bg-white mobile:h-90pxr'>
+        {order === 1 && <Image fill src={imageUrl} alt='카드 이미지' />}
+      </div>
       <div className='w-220pxr text-center'>
         <h2 className='secondBox truncate text-28pxr font-body1' title={name}>
           {name}
